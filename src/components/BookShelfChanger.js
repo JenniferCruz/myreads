443e4ec5a56a03b83390582ede655998fb69449e
JCruz <jennifer.cruzz@gmail.com>
2017-12-28 20:18:20 -0600

    feat: Implement on select change handler

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -4,10 +4,10 @@ class BookShelfChanger extends React.Component {
 
     render() {
         return (<div className="book-shelf-changer">
-                <select onChange={event => this.props.onChangeBookShelf(event.target.value)}>
+                <select onChange={event => {this.props.onChangeBookShelf(this.props.book, event.target.value)}}>
                     <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
+                    <option value="reading">Currently Reading</option>
+                    <option value="want">Want to Read</option>
                     <option value="read">Read</option>
                     <option value="none">None</option>
                 </select>
--- src/components/BookView.js
+++ src/components/BookView.js
@@ -9,7 +9,7 @@ class BookView extends React.Component {
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.image }}></div>
-                    <BookShelfChanger/>
+                    <BookShelfChanger onChangeBookShelf={this.props.onChangeBookShelf} book={book}/>
                 </div>
                 <div className="book-title">{book.title}</div>
                 <div className="book-authors">{book.author}</div>
