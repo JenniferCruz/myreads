bf0b61696523e00306b06b28ebffaf2d91a60168
JCruz <jennifer.cruzz@gmail.com>
2017-12-29 11:12:20 -0600

    fix: Display the right book shelf on each book's select dropdown

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function BookShelfChanger(props) {
+    const book = props.book;
     return (<div className="book-shelf-changer">
-                <select onChange={event => props.onChangeBookShelf(props.book, event.target.value)}>
+                <select value={book.shelf} onChange={event => props.onChangeBookShelf(book, event.target.value)}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
