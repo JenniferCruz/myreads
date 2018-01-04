a966524a28811f73a4ed9a1eed4a8f19b174930e
JCruz <jennifer.cruzz@gmail.com>
2018-01-04 17:25:31 -0600

    fix: Allow user to select 'currently leading' in Search results books

--- src/components/BookShelfChanger.js
+++ src/components/BookShelfChanger.js
@@ -3,7 +3,7 @@ import React from 'react'
 function BookShelfChanger(props) {
     const book = props.book;
     return (<div className="book-shelf-changer">
-                <select value={book.shelf} onChange={event => props.onChangeBookShelf(book, event.target.value)}>
+                <select value={book.shelf || 'none'} onChange={event => props.onChangeBookShelf(book, event.target.value)}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
